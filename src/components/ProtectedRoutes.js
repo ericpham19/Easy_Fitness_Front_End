605a0260f86112ecd8c9d6f06867cfa2b65abdb6
Duy Pham <ericpham19@outlook.com>
2022-11-26 03:43:09 +0500

    Show toast for noauth request

--- src/components/ProtectedRoutes.js
+++ src/components/ProtectedRoutes.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const ProtectedRoutes = ({ children }) => {
-  const userToken = useSelector((state) => state.userToken)
+  const userToken = useSelector((state) => state.user.userToken)
   if (!userToken) {
+    toast.warn("Please login first!")
     return <Navigate to="/login" replace />;
   }
 
